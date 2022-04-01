386bfea3ca5400982a2dd573f7f3c9cd9a3dee07
Sirisha K <siresha.k99@gmail.com>
2022-04-01 21:04:09 +0530

    sending naika to git

---
+++ src/main/java/naika_lipstik/controllers/SampleController.java
@@ -0,0 +1,28 @@
+package naika_lipstik.controllers;
+
+import com.agapsys.rcf.ActionRequest;
+import com.agapsys.rcf.ActionResponse;
+import com.agapsys.rcf.Controller;
+import com.agapsys.rcf.HttpMethod;
+import com.agapsys.rcf.WebAction;
+import com.agapsys.rcf.WebActions;
+import com.agapsys.rcf.WebController;
+import java.io.IOException;
+
+@WebController("sample")
+public class SampleController extends Controller {
+
+    @WebActions({
+        @WebAction(httpMethods = HttpMethod.GET, mapping = "/getObject"),
+        @WebAction(httpMethods = HttpMethod.GET, mapping = "/")
+    })
+    public String getObject(ActionRequest req) {
+        return "Hello world!";
+    }
+
+    @WebAction
+    public void get(ActionRequest request, ActionResponse response) throws IOException {
+        response.getServletResponse().getWriter().print("Hello world!");
+    }
+
+}
